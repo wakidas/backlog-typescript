131f78671e895deb93268cc6b1ee6ecb7fdcb66d
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-02-05 22:20:23 +0900

    webpack.configの不要記述削除

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,5 @@
 const { resolve } = require("path");
 
-// module.exports = {
-//   mode: "development",
-//   target: "node",
-//   entry: resolve(__dirname, "backlog.js"),
-//   output: {
-  //     filename: "index.js",
-  //     path: resolve(__dirname, "dist1"),
-  //   },
-  //   module: {
-    //     rules: [
-      //       {
-        //         test: /\.m?js$/,
-        //         exclude: /node_modules/,
-        //         use: {
-          //           loader: "babel-loader",
-          //         },
-          //       },
-          //     ],
-          //   },
-          // }
 module.exports = {
   target: "node",
   mode: process.env.NODE_ENV || "development",
